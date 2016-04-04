fe33579153eccb721ab7e98776fe29ba54f610f2
doraki <dorajambor@gmail.com>
2016-04-03 22:59:22 -0400

    Fix the mess

--- display.js
+++ display.js
@@ -2,52 +2,45 @@
 // My first attempts to get into JS
 // Code is used for my personal website
 
-var Thoughts_Dropdown = ["Recurse Center"];
-var Portfolio_Dropdown = ["OpenCV", "Website", "Neural Networks", "Random"];
-
 // hide social icons with scroll down
 $(window).scroll(function() {
 	if ($(this).scrollTop()>0)
 	{
-		$('.head').fadeOut();
+		$('.icon').fadeOut();
 	}
 	else
 	{
-		$('.head').fadeIn();
+		$('.icon').fadeIn();
 	}
 });
 
+// About -> hide home pic and bring in about section
 function dropFunc1() {
-	 document.getElementById("about").classList.toggle('show');
+	$('#image').fadeOut();
+	$(".post-image-caption").fadeOut();
 }
+function homeFunc() {
+	$('#image').fadeIn();
+	$(".post-image-caption").fadeIn();
+}
+// Portfolio section
 function dropFunc2() {
-	 document.getElementById("portfolio").classList.toggle('show');
+	// document.getElementById("portfolio").classList.toggle('show');
 }
 function dropFunc3() {
-	 document.getElementById("thoughts").classList.toggle('show');
+	document.getElementById("thoughts").classList.toggle('show');
 }
 
 window.onclick = function(event){
-	if (!event.target.matches('.menu')) {
-		var dropdowns = document.getElementsByClassName("dropdownList0");
-		for (var i=0; i <dropdowns.length; i++) {
-			var openDropdown = dropdowns[i];
-			if (openDropdown.classList.contains('show')) {
-				openDropdown.classList.remove('show');
-			}
-		}
-	}
-}
-window.onclick = function(event){
-	if (!event.target.matches('.menu')) {
-		var dropdowns = document.getElementsByClassName("dropdownList1");
-		for (var i=0; i <dropdowns.length; i++) {
-			var openDropdown = dropdowns[i];
-			if (openDropdown.classList.contains('show')) {
-				openDropdown.classList.remove('show');
-			}
-		}
-	}
+	// if (!event.target.matches('.menu')) {
+	// 	var dropdowns = document.getElementsByClassName("dropdownList1");
+	// 	for (var i=0; i <dropdowns.length; i++) {
+	// 		var openDropdown = dropdowns[i];
+	// 		if (openDropdown.classList.contains('show')) {
+	// 			openDropdown.classList.remove('show');
+	// 		}
+	// 	}
+	// }
 }
 window.onclick = function(event){
 	if (!event.target.matches('.menu')) {
@@ -60,5 +53,3 @@ window.onclick = function(event){
 		}
 	}
 }
-
-
